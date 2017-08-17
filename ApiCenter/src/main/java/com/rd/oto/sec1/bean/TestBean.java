7a9e2abf4e09b08545f6cac4a2286461ad59b47e
Lin <linzhongtai@gengee.cn>
2017-08-17 20:29:17 +0800

    [Ru-Server]init project

---
+++ ApiCenter/src/main/java/com/rd/oto/sec1/bean/TestBean.java
@@ -0,0 +1,7 @@
+package com.rd.oto.sec1.bean;
+
+/**
+ * Created by LinZhongtai <linzhongtai@gengee.cn>
+ */
+public class TestBean {
+}
---
+++ ApiCenter/src/main/java/com/rd/oto/sec1/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.rd.oto.sec1.mapper;
+
+/**
+ * Created by LinZhongtai <linzhongtai@gengee.cn>
+ */
+public interface TestMapper {
+
+    String getMysqlVersion();
+}
